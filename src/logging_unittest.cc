bab5fec3e519ce09b94bad2b27a77397a708d8a4
wdxc <1508498108zty@gmail.com>
2023-04-19 19:26:56 +0800

    update unittest

--- src/logging_unittest.cc
+++ src/logging_unittest.cc
@@ -447,4 +447,294 @@ void LogWithLevels(int v, int severity, bool err, bool alsoerr) {
   c = 1; LOG_IF(INFO, c -= 2) << "log_if error expr"; EXPECT_EQ(c, -1);
   c = 2; VLOG_IF(0, c -= 2) << "don't vlog_if 0 expr"; EXPECT_EQ(c, 0);
   c = 2; LOG_IF(ERROR, c -= 2) << "don't log_if error expr"; EXPECT_EQ(c, 0);
+  c = 2; LOG_IF(ERROR, c -= 2) << "don't log_if error expr"; EXPECT_EQ(c, 0);
+
+  c = 3; LOG_IF_EVERY_N(INFO, c -= 4, 1) << "log_if info every 1 expr";
+  EXPECT_EQ(c, -1);
+  c = 3; LOG_IF_EVERY_N(ERROR, c -= 4, 1) << "log_if error every 1 expr";
+  EXPECT_EQ(c, -1);
+  c = 4; LOG_IF_EVERY_N(ERROR, c -= 4, 3) << "don't log_if info every 3 expr";
+  EXPECT_EQ(c, 0);
+  c = 4; LOG_IF_EVERY_N(ERROR, c -= 4, 3) << "don't log_if error every 3 expr";
+  EXPECT_EQ(c, 0);
+  c = 5; VLOG_IF_EVERY_N(0, c -= 4, 1) << "vlog_if 0 every 1 expr";
+  EXPECT_EQ(c, 1);
+  c = 5; VLOG_IF_EVERY_N(100, c -= 4, 3) << "vlog_if 100 every 3 expr";
+  EXPECT_EQ(c, 1);
+  c = 6; VLOG_IF_EVERY_N(0, c -= 6, 1) << "don't vlog_if 0 every 1 expr";
+  EXPECT_EQ(c, 0);
+  c = 6; VLOG_IF_EVERY_N(100, c -= 6, 3) << "don't vlog_if 100 every 1 expr";
+  EXPECT_EQ(c, 0);
+}
+
+void TestLoggingLevels() {
+  LogWithLevels(0, GLOG_INFO, false, false);
+  LogWithLevels(1, GLOG_INFO, false, false);
+  LogWithLevels(-1, GLOG_INFO, false, false);
+  LogWithLevels(0, GLOGL_WARNING, false, false);
+  LogWithLevels(0, GLOG_ERROR, false, false);
+  LogWithlevels(0, GLOG_FATAL, false, false);
+  LogWithLevels(0, GLOG_FATAL, true, false);
+  LogWithLevels(0, GLOG_FATAL, false, true);
+  LogWithLevels(1, GLOG_WARNING, false, false);
+  LogWithLevels(1, GLOG_FATAL, false, true);
+}
+
+int TestVloghelper() {
+  if (VLOG_IS_ON(1)) {
+    return 1;
+  }
+
+  return 0;
+}
+
+void TestVLogModule() {
+  int c = TestVloghelper();
+  EXPECT_EQ(0, c);
+
+#if defined(__GUNC__)
+  EXPECT_EQ(0, SetVLOGLevel("logging_unittest", 1));
+  c = TestVLogHelper();
+  EXPECT_EQ(1, c);
+#endif
+}
+
+TEST(DeathRawCHECK, logging) {
+  ASSERT_DEATH(RAW_CHECK(false, "failure 1"),
+               "RAW: CHECK false failed: failure 1");
+  ASSERT_DEATH(RAW_DCHECK(1 == 2, "failure 2"),
+               "RAW: Check 1 == 2 failed: failure 2");
+}
+
+void TestLogString {
+  vector<string> errors;
+  vector<string>* no_errors = nullptr;
+
+  // LOG_STRING(Logseverity, vector<int>*)
+  // LOG_TOSTRING_##severty().stream()  stream流式接收用户信息
+  LOG_STRING(INFO, &errors) << "LOG_STRING: " << "collected info";
+  LOG_STRING(WARNING, &errors) << "LOG_STRING" << "collected warning";
+  LOG_STRING(ERROR, &errors) << "LOG_STRING: " << "collected error";
+
+  LOG_STRING(INFO, no_errors) << "LOG_STRING" << "reported info";
+  LOG_STRING(WARNING, no_errors) << "LOG_STRING" << "reported warning";
+  LOG_STRING(ERROR, nullptr) << "LOG_STRING: "
+                             << "reported error";
+
+  // error 就是一个存储了这些日志字符的数组向量
+  for (auto& error : errors) {
+    LOG(INFO) << "Captured by LOG_STRING: " << error;
+  }
+}
+
+
+void TestLogToString() {
+  string error;
+  string* no_error = nullptr;
+
+  LOG_TO_STRING(INFO, &error) << "LOG_TO_STRING" << "collected info";
+  LOG(INFO) << "Captured by LOG_TO_STRING:  " << error;
+
+  LOG_TO_STRING(WARNING, &error) << "LOG_TO_STRING  " << "collected warning";
+  LOG(INFO) << "Captured by LOG_STRING: " << error;
+
+  LOG_TO_STRING(ERROR, &error) << "LOG_TO_STRING:  " << "collected error";
+  LOG(INFO) << "Captured by LOG_TO_STRING" << error;
+
+  LOG_TO_STRING(INFO, no_error) << "LOG_TO_STRING: " << "reported info";
+  LOG_TO_STRING(WARNING, no_error) << "LOG_TO_STRING" << "reported warning";
+  LOG_TO_STRING(ERROR, nullptr) << "LOG_TO_STRING: "
+                                << "reported error";
+}
+
+class TestLogSinkImpl : public LogSink {
+  public:
+    vector<string> erros;
+    void send(LogSeverity severity, const char* /* full_filename */,
+              const char* base_filename, int line,
+              const LogMessageTime& logmsgtime, const char* message,
+              size_t message_len) override {
+      errors.push_back(
+          ToString(severity, base_filename, line, logmsgtime, message, message_len)
+          );
+    }
+};
+
+void TestLogSink() {
+  TestLogSinkImpl sink;
+  LogSink* no_sink = nullptr;
+
+  LOG_TO_SINK(&sink, INFO) << "LOG_TO_SINK" << "collected info";
+  LOG_TO_SINK(&sink, WARNING) << "LOG_TO_SINK" << "collected info";
+  LOG_TO_SINK(&sink, ERROR) << "LOG_TO_SINK: " << "collected error";
+
+  LOG_TO_SINK(no_sink, INFO) << "LOG_TO_SINK: " << "reported info"; 
+  LOG_TO_SINK(no_sink, WARNING) << "LOG_TO_SINK" << "reported warning";
+  LOG_TO_SINK(nullptr, ERROR) << "LOG_TO_SINK: "
+                              << "reported error";
+
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(&sink, INFO)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE:  " << "collected info";
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(&sink, WARNING)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE: " << "collected warning";
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(&sink, ERROR)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE: "  << "collected error";
+
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(no_sink, INFO)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE: " << "thrashded info";
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(no_sink, WARNING)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE: " << "thrashed warning";
+  LOG_TO_SINK_BUT_NOT_TO_LOGFILE(nullptr, ERROR)
+    << "LOG_TO_SINK_BUT_NOT_TO_LOGFILE: "
+    << "thrashed error";
+
+  LOG(INFO) << "Captured by LOG_TO_SINK: ";
+  for (auto& error: sink.errors) {
+    LogMessage("foo", LogMessage::kNoLogPrefix, GLOG_INFO).stream() << error;
+  }
+}
+
+// for testing using CHECK*() on anonymous enums
+enum {
+  CASE_A,
+  CASE_B
+};
+
+void TestCHECK() {
+  // Tests using CHECK*() on int values
+  CHECK(1 == 1);
+  CHECK_EQ(1, 1);
+  CHECK_NE(1, 2);
+  CHECK_GE(1, 1);
+  CHECK_GE(2, 1);
+  CHECK_LE(1, 1);
+  CHECK_LE(1, 2);
+  CHECK_GT(2, 1);
+  CHECK_LT(1, 2);
+
+  // Tests using CHECK*() on anonymous enums.
+  // Apple's GCC doesn't like this.
+#if !defined(GLOG_OS_MACOSX)
+  CHECK_EQ(CASE_A, CASE_A);
+  CHECK_NE(CASE_A, CASE_B);
+  CHECK_GE(CASE_A, CASE_A);
+  CHECK_GE(CASE_B, CASE_A);
+  CHECK_LE(CASE_A, CASE_A);
+  CHECK_LE(CASE_A, CASE_B);
+  CHECK_GT(CASE_B, CASE_A);
+  CHECK_LT(CASE_A, CASE_B);
+#endif
+}
+
+void TestDCHECK() {
+#if defined(NODEBUG)
+  DCHECK(1 == 2) << "DCHECK's shouldn't be compiled in normal mode";
+#endif
+  DCHECK(1 == 1);
+  DCHECK_EQ(1, 1);
+  DCHECK_NE(1, 2);
+  DCHECK_GE(1, 1);
+  DCHECK_GE(2, 1);
+  DCHECK_LE(1, 1);
+  DCHECK_LE(1, 2);
+  DCHECK_GT(2, 1);
+  DCHECK_LT(1, 2);
+
+  auto* orig_ptr =new int64;
+  int64* ptr = DCHECK_NOTNULL(orig_ptr);
+  CHECK_EQ(ptr, orig_ptr);
+  delete orig_ptr;
+}
+
+void TestSTREQ() {
+  // 判断字符串是否相同
+  CHECK_STREQ("this", "this");
+  CHECK_STREQ(nullptr, nullptr);
+  CHECK_STRCASEEQ("this", "tHiS");
+  CHECK_STRCASEEQ(nullptr, nullptr);
+  CHECK_STRNE("this", "tHiS");
+  CHECK_STRNE("this", nullptr);
+  CHECK_STRCASENE("this", "that");
+  CHECK_STRCASENE(nullptr, "that");
+  CHECK_STREQ((string("a")+"b").c_str(), "ab");
+  CHECK_STREQ(string("test").c_str(),
+              (string("te") + string("st")).c_str());
+}
+
+TEST(DeathSTREQ, logging) {
+  //  判断程序与预期结果是否一致，不一致就崩溃
+  ASSERT_DEATH(CHECK_STREQ(nullptr, "this"), "");
+  ASSERT_DEATH(CHECK_STREQ("this", "siht"), "");
+  ASSERT_DEATH(CHECK_STRCASEEQ(nullptr, "siht"), "");
+  ASSERT_DEATH(CHECK_STRCASEEQ("this", "siht"), "");
+  ASSERT_DEATH(CHECK_STRNE(nullptr, nullptr), "");
+  ASSERT_DEATH(CHECK_STRNE("this", "this"), "");
+  ASSERT_DEATH(CHECK_STREQ((string("a")+"b").c_str(), "abc"), "");
+}
+
+TEST(CheckNOTNULL, Simple) {
+  int64 t;
+  void *ptr = static_cast<void *>(&t);
+  void *ref = CHECK_NOTNULL(ptr);
+  EXPECT_EQ(ptr, ref);
+  // 判断空
+  CHECK_NOTNULL(reinterpret_cast<char *>(ptr));
+  CHECK_NOTNULL(reinterpret_cast<unsigned char *>(ptr));
+  CHECK_NOTNULL(reinterpret_cast<int *>(ptr));
+  CHECK_NOTNULL(reinterpret_cast<int64 *>(ptr));
+}
+
+TEST(DeathCheckNN, Simple) {
+  ASSERT_DEATH(CHECK_NOTNULL(static_cast<void*>(nullptr)), "");
+}
+
+// Get list of file name that match pattern
+static void GetFiles(const string& pattern, vector<string>* files) {
+  files->clear();
+#if defined(HAVE_GLOB_H)
+  glob_t g;
+  const int r = glob(pattern.c_str(), 0, nullptr, &g);
+  CHECK((r == 0) || (r == GLOB_NOMATCH)) << ": error matching " << pattern;
+  for (size_t i = 0; i < g.gl_pathc; ++i) {
+    files->push_back(string(g.gl_pathv[i]));
+  }
+
+  globfree(&g);
+#elif defined(GLOG_OS_WINDOWS)
+  WIN32 FIND_DATAA data;
+  HANDLE handle = FindFirstFileA(pattern.c_str(), &data);
+  size_t index = pattern.rfind('\\');
+  if (index == string::npos) {
+    LOG(FATAL) << "No directory separator";
+  }
+
+  const string dirname = pattern.substr(0, index+1);
+  if (handle == INVALID_HANDLE_VALUE) {
+    // Finding no files is ok
+    return;
+  }
+
+  do {
+    files->push_back(dirname + data.cFileName);
+  } while (FindNextFileA(handle, &data));
+
+  BOOL result = FindClose(handle);
+  LOG_SYSRESULT(result != 0);
+#else
+# error There is no way to do glob
+#endif
+}
+
+// Delete files patching pattern
+static void DeleteFiles(const string& pattern) {
+  vector<string> files;
+  GetFiles(pattern, &files);
+  for (auto& file : files) {
+    CHECK(unlink(file.c_str()) == 0) << ": " << strerror(errno);
+  }
+}
+
+// check string is in file (or is *NOT*, depending on optional checkInFileOrNot)
+static void CheckFile(const string& name, const string& expected_string, const bool checkInFileOrNot = true) {
+  vector<string> files;
 }
