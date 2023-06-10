9e9f3e1c10aaef7b5c0909069e1adf7f7728fe23
wdxc <1508498108zty@gmail.com>
2023-06-10 15:44:05 +0800

    completely

--- src/logging_striptest10.cc
+++
@@ -1,4 +0,0 @@
-#define GOOGLE_STRIPG_LOG 10
-
-// Include the actual test
-#include "logging_striptest_main"
--- src/logging_striptest2.cc
+++
@@ -1,4 +0,0 @@
-#define GOOGLE_STRIP_LOG
-
-// Include the actual test
-#include "logging_striptest_main.cc"
