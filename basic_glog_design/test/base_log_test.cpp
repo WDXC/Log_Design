4e4e511c82253f2d1f0361081b083dec1edbac80
WDXC <1508498108zty@gmail.com>
2023-06-10 14:55:34 +0800

    modify config

--- basic_glog_design/test/base_log_test.cpp
+++ basic_glog_design/test/base_log_test.cpp
@@ -1,5 +1,5 @@
 #include "base_log.h"
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 #include <gmock/gmock.h>
 #include <iostream>
 
@@ -14,21 +14,3 @@ int main() {
     EXPECT_EQ(RUN_ALL_TESTS(), 0);
 }
 
-static void NoAllocNewHook() {
-  LOG(FATAL) << "unexpected new";
-}
-
-struct NewHook {
-  NewHook() {
-    g_new_hook = &NoAllocNewHook;
-  }
-  ~NewHook() { g_new_hook = nullptr; }
-};
-
-TEST(DeathNoAllocNewHook, logging) {
-    // tests that NewHook used below works
-    NewHook new_hook;
-    ASSERT_DEATH({
-
-            })
-}
